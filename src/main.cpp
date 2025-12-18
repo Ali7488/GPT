cef38712b2e632935ec3d470ec76b088ba1462c4
Mohannad Ali <mohannad.eali@email.com>
2025-12-18 02:34:13 -0600

    Set up CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "util.h"
 
 int main()
 {
+    print_util_message();
     std::cout << "Hello World\n";
     return 0;
 }
---
+++ src/util.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "util.h"
+
+void print_util_message()
+{
+    std::cout << "This is a utility function.\n";
+}
